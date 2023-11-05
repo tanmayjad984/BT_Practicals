0db903211b573685f2b8156b9d9cb01cf76bff3d
Tanmay Jadhav <118904761+tanmayjad984@users.noreply.github.com>
2023-11-05 23:15:27 +0530

    Update Studentdata.sol

--- Studentdata.sol
+++ Studentdata.sol
@@ -1,31 +1,42 @@
-contract StudentData{
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-struct Student{ int rollNo;
-string fName; string lName; int marks;
-}
+contract StudentData {
+    struct Student {
+        uint256 studentID;
+        string name;
+        uint256 age;
+    }
 
-address owner;  int public stdCount =0; mapping(int => Student) public stdRecords;
+    Student[] public students;
+    address public owner;
 
-modifier onlyOwner{ require (owner == msg.sender);
-_;
+    constructor() {
+        owner = msg.sender;
+    }
 
-}
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can perform this action.");
+        _;
+    }
 
-constructor(){
+    function addStudent(uint256 _studentID, string memory _name, uint256 _age) public onlyOwner {
+        Student memory newStudent = Student(_studentID, _name, _age);
+        students.push(newStudent);
+    }
 
-owner=msg.sender;
-}
+    function getStudentCount() public view returns (uint256) {
+        return students.length;
+    }
 
-function addNewRecords (int _rollNo, string memory _fName, string memory _lName, int
-_marks) public onlyOwner{ stdCount=stdCount+1;  stdRecords
- 
-[stdCount]=Student
-(_rollNo,_fName,_lName,_marks);
+    function getStudent(uint256 index) public view returns (uint256, string memory, uint256) {
+        require(index < students.length, "Student not found");
+        Student storage student = students[index];
+        return (student.studentID, student.name, student.age);
+    }
 
-}
-function bonusMarks(int _bonus) public onlyOwner{
-stdRecords[stdCount].marks=stdRecords[stdCount].marks + _bonus;
-}
-fallback () external payable{
-}
+    // Fallback function to receive Ether
+    receive() external payable {
+        // This function allows the contract to accept Ether without any specific logic.
+    }
 }
